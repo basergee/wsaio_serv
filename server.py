51894d6ef8b1300d44cbf40c8d581cb43920f6dd
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2023-01-16 21:27:07 +0500

    Перенес обработку websocket на другой адрес. Изменил реакцию на сообщения

--- server.py
+++ server.py
@@ -5,37 +5,9 @@ from aiohttp import web
 WS_FILE = os.path.join(os.path.dirname(__file__), "websocket.html")
 
 
-async def wshandler(request: web.Request):
-    resp = web.WebSocketResponse()
-    available = resp.can_prepare(request)
-    if not available:
-        with open(WS_FILE, "rb") as fp:
-            return web.Response(body=fp.read(), content_type="text/html")
-
-    await resp.prepare(request)
-
-    await resp.send_str("Welcome!!!")
-
-    try:
-        print("Someone joined.")
-        for ws in request.app["sockets"]:
-            await ws.send_str("Someone joined")
-        request.app["sockets"].append(resp)
-
-        async for msg in resp:
-            if msg.type == web.WSMsgType.TEXT:
-                for ws in request.app["sockets"]:
-                    if ws is not resp:
-                        await ws.send_str(msg.data)
-            else:
-                return resp
-        return resp
-
-    finally:
-        request.app["sockets"].remove(resp)
-        print("Someone disconnected.")
-        for ws in request.app["sockets"]:
-            await ws.send_str("Someone disconnected.")
+async def index_page_handler(request: web.Request):
+    with open(WS_FILE, "rb") as fp:
+        return web.Response(body=fp.read(), content_type="text/html")
 
 
 # Принимает POST-запрос на создание новости и рассылает текст новости всем
@@ -52,6 +24,35 @@ async def post_news_handler(request: web.Request):
     return web.Response(text=data['newstext'])
 
 
+# Поддерживает соединение
+async def websocket_handler(request: web.Request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    try:
+        # Сохраним новое соединение, чтобы была возможность рассылать новости
+        request.app["sockets"].append(ws)
+
+        async for msg in ws:
+            if msg.type == web.WSMsgType.TEXT:
+                if msg.data == 'close':
+                    await ws.close()
+                else:
+                    # Любую строку отправляем назад. Это сделано для проверки
+                    # соединения между клиентом и сервером.
+                    await ws.send_str(msg.data)
+            elif msg.type == web.WSMsgType.ERROR:
+                print('ws connection closed with exception %s' %
+                      ws.exception())
+
+        print('websocket connection closed')
+
+        return ws
+    finally:
+        # в случае ошибки удаляем вновь добавленное соединение
+        request.app["sockets"].remove(ws)
+
+
 async def on_shutdown(app: web.Application):
     for ws in app["sockets"]:
         await ws.close()
@@ -60,8 +61,9 @@ async def on_shutdown(app: web.Application):
 def init():
     app = web.Application()
     app["sockets"] = []
-    app.add_routes([web.get('/', wshandler),
-                    web.post('/news', post_news_handler)])
+    app.add_routes([web.get('/', index_page_handler),
+                    web.post('/news', post_news_handler),
+                    web.get("/ws", websocket_handler)])
     app.on_shutdown.append(on_shutdown)  # on_shutdown опишем позже
     return app
 
